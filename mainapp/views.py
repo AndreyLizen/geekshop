dedf058e9e0b5f4191547b854bfb9f2b1106aa44
Andrey L <xipnex-1@yandex.ru>
2021-01-28 19:04:52 +0300

    003_промежуточный

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,16 +4,16 @@ from django.shortcuts import render
 
 
 def main(request):
-    context = {
-        'title': "тестовый заголовок",
-        'products': [
-            {'name': 'Товар1', 'price': "1000руб."},
-            {'name': 'Товар2', 'price': "2000руб."},
-            {'name': 'Товар3', 'price': "3000руб."},
-        ]
-    }
-    return render(request, 'mainapp/index.html', context)
-
+    # context = {
+    #     'title': "тестовый заголовок",
+    #     'products': [
+    #         {'name': 'Товар1', 'price': "1000руб."},
+    #         {'name': 'Товар2', 'price': "2000руб."},
+    #         {'name': 'Товар3', 'price': "3000руб."},
+    #     ]
+    # }
+    # return render(request, 'mainapp/index.html', context)
+    return render(request, 'mainapp/index.html')
 
 def products(request):
     return render(request, 'mainapp/products.html')
