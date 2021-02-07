4ed4c9470755a1487553706178ba3e20bf645f3a
Andrey L <xipnex-1@yandex.ru>
2021-02-08 00:58:37 +0300

    005_final

--- basket/views.py
+++ basket/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import HttpResponseRedirect
+from django.shortcuts import HttpResponseRedirect, get_object_or_404
 # from django.contrib.auth.decorators import login_required
-from django.template.loader import render_to_string
-from django.http import JsonResponse
+# from django.template.loader import render_to_string
+# from django.http import JsonResponse
 
 from mainapp.models import Product
 from basket.models import Basket
@@ -29,18 +29,18 @@ def basket_remove(request, id):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
-def basket_edit(request, id, quantity):
-    if request.is_ajax():
-        quantity = int(quantity)
-        basket = Basket.objects.get(id=int(id))
-        if quantity > 0:
-            basket.quantity = quantity
-            basket.save()
-        else:
-            basket.delete()
-        baskets = Basket.objects.filter(user=request.user)
-        context = {
-            'baskets': baskets,
-        }
-        result = render_to_string('basket/basket.html', context)
-        return JsonResponse({'result': result})
+# def basket_edit(request, id, quantity):
+#     if request.is_ajax():
+#         quantity = int(quantity)
+#         basket = Basket.objects.get(id=int(id))
+#         if quantity > 0:
+#             basket.quantity = quantity
+#             basket.save()
+#         else:
+#             basket.delete()
+#         baskets = Basket.objects.filter(user=request.user)
+#         context = {
+#             'baskets': baskets,
+#         }
+#         result = render_to_string('basket/basket.html', context)
+#         return JsonResponse({'result': result})
