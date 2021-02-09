edcfe119b79a391ac5d3c59f6bfca28eb7110c96
Andrey L <xipnex-1@yandex.ru>
2021-02-10 00:37:26 +0300

    007_Добавлена таблица для просмотра категорий

--- adminapp/views.py
+++ adminapp/views.py
@@ -2,19 +2,24 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import user_passes_test
 
+from mainapp.models import ProductCategory
 from authapp.models import User
 from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
-
 
 @user_passes_test(lambda u: u.is_superuser, login_url='/')
 def index(request):
     return render(request, 'adminapp/index.html')
 
-
 @user_passes_test(lambda u: u.is_superuser)
 def admin_users_read(request):
     context = {'users': User.objects.all()}
     return render(request, 'adminapp/admin-users-read.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_categories_read(request):
+    context = {'categories': ProductCategory.objects.all()}
+    return render(request, 'adminapp/admin-categories-read.html', context)
 
 
 @user_passes_test(lambda u: u.is_superuser)
